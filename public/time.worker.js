ba841b67a822cc0d9fa5d5435025b75f81190617
ZiuChen <457353192@qq.com>
2022-09-17 21:14:01 +0800

    refactor: 将time.js相关代码最小化 压缩插件体积

--- public/time.worker.js
+++ public/time.worker.js
@@ -1,21 +1,2 @@
-// author: inu1255
-
-const apis = {
-  sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
-  }
-}
-
-onmessage = (event) => {
-  const data = event.data
-  if (!data) return
-  const { cb, method, args } = data
-  if (!apis[method]) {
-    postMessage({ cb, err: 'no such method' })
-    return
-  }
-  apis[method].apply(null, args).then(
-    (res) => postMessage({ cb, data: res }),
-    (err) => postMessage({ cb, err })
-  )
-}
+// time.worker.js author: inu1255
+const apis={sleep(ms){return new Promise(resolve=>setTimeout(resolve,ms))}};onmessage=event=>{const data=event.data;if(!data)return;const{cb,method,args}=data;if(!apis[method]){postMessage({cb:cb,err:"no such method"});return}apis[method].apply(null,args).then(res=>postMessage({cb:cb,data:res}),err=>postMessage({cb:cb,err:err}))};
