b95de9f4f1f2057a65ade19e59cafb8447b32efc
ZiuChen <457353192@qq.com>
2022-09-20 21:42:45 +0800

    revert: 暂不进行默认设置和本地设置的diff

--- src/global/readSetting.js
+++ src/global/readSetting.js
@@ -1,27 +1,5 @@
 import restoreSetting from './restoreSetting'
-import defaultSetting from '../data/setting.json'
-import { pointToObj } from '../utils'
 
-defaultSetting = pointToObj(defaultSetting)
-
-// 比较默认设置与当前设置
-function compareSetting() {
-  const setting = utools.dbStorage.getItem('setting') || restoreSetting()
-  // 如果默认设置中新增了某个设置项，为本地设置添加该项
-  for (const key in defaultSetting) {
-    if (setting[key] === undefined) {
-      setting[key] = defaultSetting[key]
-    }
-  }
-  // 如果默认设置中删除了某个设置项，删除本地设置中的该项
-  for (const key in setting) {
-    if (defaultSetting[key] === undefined) {
-      delete setting[key]
-    }
-  }
-  return setting
-}
-
-const setting = compareSetting()
+const setting = utools.dbStorage.getItem('setting') || restoreSetting()
 
 export default setting
